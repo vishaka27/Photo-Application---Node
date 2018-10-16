2979765b90d1940b2bfc9e6340161d32f8e54117
vishaka <veenu.n2009@gmail.com>
2018-10-16 23:14:32 +0530

    fix: modified server js to send data from external url

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
 const app = express();
-const port = 5001;
 
-app.get('/home', (req, res) => {
-    res.send('Welcome to HomePage');
+const request = require('request');
+
+const port = 5000;
+
+const url = 'https://jsonplaceholder.typicode.com/photos';
+
+app.get('/', (req, res, next) => {
+    request({
+        url: url,
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
