d8a1cf23845ac404fe71fe5a6a784c5a5a54ccca
vishaka <veenu.n2009@gmail.com>
2018-10-18 17:02:20 +0530

    fix: added header data

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ global.appRoot = path.resolve(__dirname);
 
 const data = require(appRoot + '/photo-list.json');
 
+const headerData = require(appRoot + '/header.json');
+
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.port || 5001;
 
 app.get('/', (req, res) => {
     res.send(data);
-})
+});
+
+app.get('/header', (req, res) => {
+    res.send(headerData);
+});
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
